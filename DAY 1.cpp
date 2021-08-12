ce9d291263bb44e3351f9da62a123cf87c593563
kganeshsaiakhil <88671874+kganeshsaiakhil@users.noreply.github.com>
2021-08-12 23:34:39 +0530

    Recursion

--- DAY 1.cpp
+++ DAY 1.cpp
@@ -1,3 +1,55 @@
+//check Bank Balance
+#include <iostream>
+
+using namespace std;
+
+class Human
+{
+  private:
+  int bankBalance;
+
+  public:
+  Human()
+  {
+    bankBalance = 1000;
+  }
+  Human(int num)
+  {
+    bankBalance = num;
+  }
+
+  string name;
+
+  void setBankBalance(int num)
+  {
+    bankBalance = num;
+  }
+
+  int getBankBalance()
+  {
+    return bankBalance;
+  }
+};
+
+int main()
+{
+  Human human1;
+  cout<<human1.getBankBalance();
+  human1.name = "Gaurav";
+  human1.setBankBalance(54000);
+
+  cout<<human1.name<<endl;
+  cout<<human1.getBankBalance();
+
+  Human human2(20000);
+  cout<<human2.getBankBalance();
+}
+
+
+
+
+
+
 //Pascal Triangle
 
 #include<iostream>
