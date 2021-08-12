b908127c3ba390aab60aadee895dbcd42e16fd2e
kganeshsaiakhil <88671874+kganeshsaiakhil@users.noreply.github.com>
2021-08-12 23:41:11 +0530

    Recursion

--- DAY 1.cpp
+++ DAY 1.cpp
@@ -45,9 +45,21 @@ int main()
   cout<<human2.getBankBalance();
 }
 
-
-
-
+//Leap Year
+#include<iostrem>
+using namespace std;
+int main()
+{
+  int a;
+  cin>>a;
+  if(a%4==0 && (a%100!=0||a%400==0 ))
+  {
+    cout<<"its a leap year"<<endl;
+  }
+  else
+  {
+    cout<<"Its not a leap year"<<endl;
+  }
 
 
 //Pascal Triangle
